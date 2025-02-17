d89a4504272e1ae4a9fc720371988195b9d8acfe
Bhone Pyae Moie <bhonepyae.it@gmail.com>
2025-02-17 23:41:40 +0700

    test

---
+++ resources/views/Webadmin/dashboard.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="Webadmin-main d-flex w-100" style="height: 100vh;">
+        
+        <div class="sidebar bg-primary text-white position-relative" style="width: 15%;">
+            <div>
+                <h1 class="text-center bg-info p-4">Home</h1>
+            </div>
+
+            <div class="d-flex flex-column w-100 justify-content-center align-items-center">
+                <a class="text-white p-4" href="#">  Dashboard</a>
+                <a class="text-white p-4" href="#">  Dashboard</a>
+                <a class="text-white p-4" href="#">  Dashboard</a>
+                <a class="text-white p-4" href="#">  Dashboard</a>
+            </div>
+
+        </div>
+        
+        <div class="bg-light" style="width: 85%;">
+
+            <div class="top-bar d-flex bg-success text-white justify-content-around align-items-center">
+                <div class="heading p-4">
+                    <h1>Welcome to KMD</h1>
+                </div>
+
+                <div class="search-bar d-flex align-items-center">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+            </div>
+            <h1>Webadmin Dashboard</h1>
+
+        </div>
+
+
+    </div>
+
+</body>
+</html>
