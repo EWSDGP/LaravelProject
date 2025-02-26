6105baecf10b643b604f365cbb8b301fa3743a90
Devis992001 <khantzaw992001@gmail.com>
2025-02-27 01:49:51 +0700

    Add department attribute to user table and modify user crud operations

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,31 +8,34 @@
                 <div class="card-header">{{ __('Show User Details') }}</div>
 
                 <div class="card-body">
-                    <a href="{{route('users.index')}}" class="btn btn-info mb-3">Back</a>
-               
-                        @csrf
+                    <a href="{{ route('users.index') }}" class="btn btn-info mb-3">Back</a>
 
-                       
-                        <div class="mt-2">
-                            <label>Name:</label>
-                            <p>{{$user->name}}</p>
-                           
-                        </div>
-                        <div class="mt-2">
-                            <label>Email:</label>
-                            <p>{{$user->email}}</p>
-                           
-                        </div>
-                        <!-- <div class="mt-2">
-                            <label>Password:</label>
-                            <p>{{$user->password}}</p>
-                            
-                        </div> -->
-                       
-                    </form>
+                    <div class="mt-2">
+                        <label><strong>Name:</strong></label>
+                        <p>{{ $user->name }}</p>
+                    </div>
 
+                    <div class="mt-2">
+                        <label><strong>Email:</strong></label>
+                        <p>{{ $user->email }}</p>
+                    </div>
 
-                   
+                    <div class="mt-2">
+                        <label><strong>Department:</strong></label>
+                        <p>{{ $user->department ? $user->department->name : 'No Department Assigned' }}</p>
+                    </div>
+
+                    <div class="mt-2">
+                        <label><strong>Roles:</strong></label>
+                        <p>
+                            @if($user->roles->isNotEmpty())
+                                {{ implode(', ', $user->roles->pluck('name')->toArray()) }}
+                            @else
+                                No Role Assigned
+                            @endif
+                        </p>
+                    </div>
+
                 </div>
             </div>
         </div>
