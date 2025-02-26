074041191bcb2246f58aacb0190bc51add0bc4d6
Min Thiha <appleopen1@gmail.com>
2025-02-26 20:43:31 +0630

    crud department

---
+++ resources/views/department/create.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Department</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body style="font-family: Arial, sans-serif; background-color: #f8f9fa; margin: 0; padding: 0;">
+    <div class="container mt-5" style="background-color: #fff; padding: 2rem; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header">{{ __('Create Department') }}</div>
+                    <div class="card-body">
+                        <a href="{{ route('departments.index') }}" class="btn btn-info mb-3">Back</a>
+                        <form action="{{ route('departments.store') }}" method="POST">
+                            @csrf
+                            <div class="mt-2">
+                                <label>Name:</label>
+                                <input type="text" name="name" class="form-control">
+                                @error('name')
+                                <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="mt-2">
+                                <button class="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
