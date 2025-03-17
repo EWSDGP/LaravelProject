4edbf274678a073014e8eff6f8c6a9cfeb0f7940
Lwin Min Thu <lminthu860@gmail.com>
2025-03-18 01:27:32 +0630

    IDK

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -33,10 +33,10 @@
                 </div>
             @endif
             @if (session('error'))
-    <div class="alert alert-danger">
-        {{ session('error') }}
-    </div>
-@endif
+                <div class="alert alert-danger">
+                    {{ session('error') }}
+                </div>
+            @endif
             
             <div class="card-header d-flex justify-content-between align-items-center">
                 <span>Category</span>
