65f039859f79eda107d2da489eb65c29824d819c
Devis992001 <khantzaw992001@gmail.com>
2025-03-29 00:25:43 +0700

    fixed profile and dynamic nav

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -318,7 +318,8 @@
                                 <div class="col-md-6">
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="current-password">
+                                        required autocomplete="current-password"
+                                        >
 
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
