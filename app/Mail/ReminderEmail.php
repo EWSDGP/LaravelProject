34d4560c9274482dd7067ab67293d268312aac3b
Lwin Min Thu <lminthu860@gmail.com>
2025-04-04 22:11:04 +0630

    email

--- app/Mail/ReminderEmail.php
+++ app/Mail/ReminderEmail.php
@@ -13,6 +13,12 @@ class ReminderEmail extends Mailable
     public function build()
     {
         return $this->subject('Reminder Notification')
-                    ->html('<p><strong>fjkadkfj</strong></p>');
+                    ->html('
+                            <p>Dear Team,</p>
+                            <p>This is a kind reminder to ensure that each staff member submits at least one post as part of our ongoing quality assurance and engagement efforts. Regular contributions are important in maintaining consistency, sharing insights, and keeping our communication flow active.</p>
+                            <p>Please make sure to complete at least one post within the designated time frame. Your participation is appreciated and plays a vital role in our team’s progress and collaboration.</p>
+                            <p>Thank you for your attention and continued commitment.</p>
+                            <p>Best regards,<br>
+                            QA Coordinator</p>');
     }
 }
